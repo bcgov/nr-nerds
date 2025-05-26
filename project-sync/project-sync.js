77cb0dceb0762859243917341879a348cde8ba59
Derek Roberts <derek.roberts@gmail.com>
2025-05-26 13:55:26 -0700

    Make sure errors cause a failure

--- project-sync/project-sync.js
+++ project-sync/project-sync.js
@@ -820,3 +820,12 @@ async function fetchRecentIssuesAndPRsGraphQL(owner, repo, sinceIso) {
     process.exit(1);
   }
 })();
+
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+  process.exit(1);
+});
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
